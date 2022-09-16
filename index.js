2e0a4ed7a597595cdef8d26eb16b883eae017dbf
Hiba <eng.lilil92@gmail.com>
2022-09-16 22:36:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue <400) {
+    return 'This one is on me!';
+  }
+  else if(someValue>400 && someValue < 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if( someValue > 2000 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue > 2500){
+    return 'No can do.';
+  }
+    
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(desiredCity){
   // Write your code here!
+  if(desiredCity == "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipValue){
   // Write your code here!
+switch(tipValue){
+  case 'generous':
+     return "Thank you so much.";
+  break;
+  case 'not as generous' :
+     return "Thank you.";
+  break;
+  default : return "Bye."
+  break;
 }
+
+
+}
+
+// scuberGreetingForFeet(199);
+// ternaryCheckCity("NYC");
+//scuberGreetingForFeet('generous');
